9479adaa4d6afa68e1cd696fe115343b49cabb74
Igor Mihailov <dr.migo@ya.ru>
2022-09-03 13:44:19 +0300

    Initial commit

---
+++ src/main/java/com/events/service/EventTicketsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.events.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/events/service/EventTicketsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
